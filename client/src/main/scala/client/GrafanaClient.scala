2c3c7e5336f9d28fac44f111cd90abe3a39295a5
Maxim <ya.mcsim1993@yandex.ru>
2021-07-17 23:54:17 +0300

    dashboard parsing

--- client/src/main/scala/client/GrafanaClient.scala
+++ client/src/main/scala/client/GrafanaClient.scala
@@ -1,22 +1,23 @@
 package scalograf
 package client
 
+import model._
+
+import io.circe.Json
+import io.circe.generic.auto._
 import sttp.client3._
 import sttp.client3.asynchttpclient.future.AsyncHttpClientFutureBackend
 import sttp.client3.circe._
-import io.circe.generic.auto._
-import model.DashboardSnippet
-
-import io.circe
 
 import scala.concurrent.Future
 
 case class GrafanaClient(url: String, authToken: String) {
-  private val backend = AsyncHttpClientFutureBackend()
+  private val backend: SttpBackend[Future, Any] = AsyncHttpClientFutureBackend()
+  private val grafanaRequest = basicRequest
+    .header("Authorization", s"Bearer $authToken")
 
   def search() = {
-    basicRequest
-      .header("Authorization", s"Bearer $authToken")
+    grafanaRequest
       .get(
         uri"$url/api/search"
           .addParam("limit", "5000")
@@ -25,7 +26,21 @@ case class GrafanaClient(url: String, authToken: String) {
       .send(backend)
   }
 
-  def
+  def dashboard(uid: String) = {
+    dashboardInner(asJsonAlways[Dashboard])(uid)
+  }
+
+  private[scalograf] def dashboardRaw(uid: String) = {
+    dashboardInner(asJsonAlways[Json])(uid)
+  }
+
+  private def dashboardInner[T](responseAs: ResponseAs[T, Any])
+                               (uid: String) = {
+    grafanaRequest
+      .get(uri"$url/api/dashboards/uid/$uid")
+      .response(responseAs)
+      .send(backend)
+  }
 
   def close() = {
     backend.close()
--- tools/src/main/scala/JsonScrapper.scala
+++ tools/src/main/scala/JsonScrapper.scala
@@ -6,19 +6,33 @@ import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.Future
+import scala.util.Failure
 
 object JsonScrapper extends App {
   val log = LoggerFactory.getLogger(this.getClass)
   val conf = ConfigFactory.parseResources("application.local.conf")
   val grafana = conf.getConfig("grafana")
 
-  val client = GrafanaClient(grafana.getString("url"), grafana.getString("token"))
+  val client =
+    GrafanaClient(grafana.getString("url"), grafana.getString("token"))
 
-  client.search().transform(
-    resp =>log.info(s"${resp.body}"),
-    err => {
-      log.error("Error", err)
-      err
+  val program = for {
+    dashboards <- client.search().map(_.body).orDie
+    _ = log.info(s"Dashboards")
+    _ = dashboards.foreach(d => log.info(s"$d"))
+    snippets <- Future.traverse(dashboards) { snippet =>
+      client.dashboardRaw(snippet.uid).map(_.body).orDie
     }
-  ).flatMap(_ => client.close())
+  } yield snippets.foreach(s => log.info(s"$s"))
+
+  program
+    .andThen {
+      case Failure(exception) => log.error("Fatal", exception)
+    }
+    .flatMap(_ => client.close())
+
+  implicit class RichFutureEither[E <: Throwable, T](val f: Future[Either[E, T]]) {
+    def orDie: Future[T] = f.map(_.toTry).flatMap(Future.fromTry)
+  }
 }
